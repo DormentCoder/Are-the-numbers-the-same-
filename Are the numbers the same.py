45884ccc17fd5f5f7dfb40f41b410e1fd990a676
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 14:02:27 +0000

    Add files via upload

---
+++ Are the numbers the same.py
@@ -0,0 +1,9 @@
+#Are the numbers the same? by Dc117 Corp.
+
+print('Are the numbers the same? V1.00')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a number: '))
+if num1 != num2:
+    print('The numbers are not the same')
+else:
+    print('The numbers are the same')
